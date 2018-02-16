c67c7e64f7d7801dfb362aeb210fb240e79a934d
jjrivasjr <jjrivasjr1@gmail.com>
2018-02-15 18:06:37 -0600

    finished

--- products.sql
+++ products.sql
@@ -1,15 +1,3 @@
-CREATE DATABASE bamazon;
-USE bamazon;
-
-CREATE TABLE PRODUCTS (
-	itemid INTEGER AUTO_INCREMENT NOT NULL,
-    productname VARCHAR(45) NOT NULL,
-	departmentname VARCHAR(45) NOT NULL,
-    price DECIMAL(10,4) NOT NULL,
-    stockquantity INTEGER(10) NOT NULL,
-    PRIMARY KEY (itemid)
-    );
-
 INSERT INTO products(ProductName,Departmentname,Price, StockQuantity)
 VALUES ("Uncharted 4", "Video Games", 49, 99, 150),
 	("DOOM", "Video Games", "Video Games", 59, 99, 200),
@@ -21,3 +9,5 @@ VALUES ("Uncharted 4", "Video Games", 49, 99, 150),
     ("Mad Max", "films", 50, 50, 50),
     ("Scrabble", "board games", 30, 50, 35),
     ("Monopoly", "board games", 19, 95, 23);
+    
+SELECT * FROM bamazon.products;
